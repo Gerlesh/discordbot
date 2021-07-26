d8bdd8636550eb1d809f4e33c42b8f94f411327b
Gerlesh <cjplanelles@gmail.com>
2021-07-25 22:00:56 -0400

    Fix update bug

--- main.py
+++ main.py
@@ -273,4 +273,4 @@ if __name__ == '__main__':
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
-    os.execvp("./update.sh", ())
+    os.execvp("./update.sh", ('./update.sh',))
