750415227e4440b4cec358922499e1ef96e86a52
Gerlesh <cjplanelles@gmail.com>
2022-02-28 13:07:03 -0500

    Automatic viewer count updating

--- cogs/Automation.py
+++ cogs/Automation.py
@@ -1,13 +1,14 @@
 import nextcord
 from nextcord.ext import commands
 
+from .utils import Autiomation_helpers
 
 class Automation(commands.Cog):
     """
     Basic server administrating
     """
 
-    def __init__(self, bot):
+    def __init__(self, bot:commands.Bot):
         self.bot = bot
         self.reactions = {'Gerlesh': '<:Gerlesh:898412274807603210>',
                           'Sukari': '<:Sukari:898412198186086431>',
@@ -30,11 +31,32 @@ class Automation(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member:nextcord.Member):
         """
-        Automatically assign the ASG Viewers role to new members
+        Automatically assign the ASG Viewers role to new members and update viewer count channel
         """
         if member.guild.id == 891583482415960074:
             await member.add_roles(nextcord.utils.get(member.guild.roles, id=895498674656911421), reason="New viewer pog")
+            viewer_count = len(member.guild.get_role(895498674656911421).members)
+            channel = self.bot.get_channel(947906376338898984)
+            await channel.edit(name="Viewer Count: "+str(viewer_count))
 
+    @commands.Cog.listener()
+    async def on_member_remove(self, member:nextcord.Member):
+        """
+        Automatically update viewer count channel when someone leaves the guild
+        """
+        if member.guild.id == 891583482415960074:
+            viewer_count = len(member.guild.get_role(895498674656911421).members)
+            channel = self.bot.get_channel(947906376338898984)
+            await channel.edit(name="Viewer Count: "+str(viewer_count))
+
+    @commands.Cog.listener()
+    async def on_connect(self):
+        """
+        Automatically update viewer count channel when bot reconnects
+        """
+        viewer_count = len(self.bot.get_guild(891583482415960074).get_role(895498674656911421).members)
+        channel = self.bot.get_channel(947906376338898984)
+        await channel.edit(name="Viewer Count: "+str(viewer_count))
 
 def setup(bot:commands.Bot):
     bot.add_cog(Automation(bot))
