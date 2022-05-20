197f42c1dfd6e63975316467aec754781135f3b7
Gerlesh <cjplanelles@gmail.com>
2022-05-20 17:43:02 -0400

    Fix bug!

--- cogs/General.py
+++ cogs/General.py
@@ -30,8 +30,7 @@ class General(commands.Cog):
         Update the bot from the github page.
         Only usable by the owner of the bot.
         """
-        appinfo = self.bot.application_info()
-        print(appinfo.owner.id)
+        appinfo = await self.bot.application_info()
         if interaction.user.id != appinfo.owner.id:
             await interaction.send("Only the bot owner can run this command.", ephemeral=True)
             return
